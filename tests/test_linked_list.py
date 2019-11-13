187dab778d69b9074e8e63536534fceaca081fe5
Jonathan Pham <jtpham4@uh.edu>
2019-11-13 01:16:34 -0600

    wip on the tests

---
+++ tests/test_linked_list.py
@@ -0,0 +1,16 @@
+import unittest
+from linked_list import LinkedList
+
+class LinkedListTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Initialize test case classes
+        :return:
+        """
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
